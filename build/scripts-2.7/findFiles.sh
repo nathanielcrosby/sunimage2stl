757e2d414b96c6892e36165a2c81689663c8e4f0
Nathaniel Crosby <nathanielcrosby@verizon.net>
2018-08-15 09:49:12 -0400

    removing dist and build

--- build/scripts-2.7/findFiles.sh
+++
@@ -1,8 +0,0 @@
-#This uses the wget function to access the web page. 
-#-r means it does so recursively so that it gets all the files in the directory
-#-np means no parent, so that it does not ascend to the parent directory and download all the images
-#-A means accept list
-#3 inputs from imageFinder.py are given that specify the date and name of the image
-echo $1
-wget -r -np -A .jp2 http://solar.physics.montana.edu/HINODE/XRT/SCIA/synop_official/$1/$2/$3/
-echo $1
